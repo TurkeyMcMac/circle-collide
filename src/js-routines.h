8b0a135b47b2c1cb296fd2d28ca1fd52996bdb36
TurkeyMcMac <jwmhjwmh@gmail.com>
2018-06-07 20:14:31 -0400

    Add a debugging guard for jsLogNum

--- src/js-routines.h
+++ src/js-routines.h
@@ -2,4 +2,8 @@ extern void jsDrawCircle(float x, float y, float radius);
 
 extern void jsDrawLine(float x1, float y1, float x2, float y2);
 
+#if HAS_DEBUG_MODE == 1
 extern void jsLogNum(int n);
+#else
+#	define jsLogNum(n)
+#endif /* HAS_DEBUG_MODE == 1 */
