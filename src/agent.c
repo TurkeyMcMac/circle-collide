aba5b25875738f8d553a0dd630a6db3e7492c27b
TurkeyMcMac <jwmhjwmh@gmail.com>
2018-06-09 07:07:52 -0400

    The repeated setting of mind_proto properties looked to unnecessary for now

--- src/agent.c
+++ src/agent.c
@@ -24,15 +24,8 @@ struct neural_net mind_proto;
 bool agent_update(struct circle *circ)
 {
 	struct agent *self = container_of(circ, struct agent, c);
-	/* If these properties aren't set each time, it seems that mind_proto is
-	 * overwritten or something. Also, mind_proto can't be static. Hopefiuly
-	 * this workaround will be obsolete eventually. */
-	mind_proto.input = AGENT_N_INPUTS;
-	mind_proto.hidden = AGENT_N_HIDDEN;
-	mind_proto.output = AGENT_N_OUTPUTS;
 	nn_bitset output = neural_net_compute(&mind_proto, self->mind,
-		vec2d_length(&circ->speed));
-	jsLogNum(1337);
+		vec2d_length(&circ->speed) * 50000.0);
 	if (output & AGENT_OUT_LEFT) {
 		self->direction += 0.1;
 	}
